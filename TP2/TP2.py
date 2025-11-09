5a16f4d041cba51575b9c1b01a92037307f92d41
SharaineMALARVIJY <sharaine.ma@gmail.com>
2025-11-09 18:43:42 +0100

    Fin TP2

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,121 +1,126 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
 
 
-# def P1(v):
-#     return -((v-2)**2)/2+1
+def P1(v):
+    return -((v-2)**2)/2+1
 
-# def P2(u):
-#     return (u-3)**2+2
+def P2(u):
+    return (u-3)**2+2
 
-# xmin, xmax, nx = -2.5, 5, 101
-# ymin, ymax, ny = -2.5, 5, 101
+xmin, xmax, nx = -2.5, 5, 101
+ymin, ymax, ny = -2.5, 5, 101
 
-# x1d = np.linspace(xmin,xmax,nx)
-# y1d = np.linspace(ymin,ymax,ny)
+x1d = np.linspace(xmin,xmax,nx)
+y1d = np.linspace(ymin,ymax,ny)
 
-# nIso = 101
-# plt.plot(P1(x1d), x1d, label='P1')
-# plt.plot(x1d, P2(x1d), label='P2')
-# plt.xlabel('Valeurs de u')
-# plt.ylabel('Valeurs de v')
-# plt.legend()
-# plt.grid()
-# plt.axis('square')
-# plt.show()
+nIso = 101
+plt.plot(P1(x1d), x1d, label='P1')
+plt.plot(x1d, P2(x1d), label='P2')
+plt.xlabel('Valeurs de u')
+plt.ylabel('Valeurs de v')
+plt.legend()
+plt.grid()
+plt.axis('square')
+plt.show()
 
 # x = [u1, u2, v1, v2, l1, l2]
 x0 = np.random.rand(6)
 
+
 def fun(X):
-    return [-2*(X[1]-X[0])+X[4],
-            2*(X[1]-X[0])-X[5]*2*(X[1]-3),
-            -2*(X[3]-X[2])+X[4]*(X[2]-2),
-            2*(X[3]-X[2])+X[5],
-            X[0]+((X[2]-2)**2)/2-1,
-            X[3]-(X[1]-3)**2-2]
+    return [
+        -2 * (X[1] - X[0]) + X[4],
+        2 * (X[1] - X[0]) - X[5] * 2 * (X[1] - 3),
+        -2 * (X[3] - X[2]) + X[4] * (X[2] - 2),
+        2 * (X[3] - X[2]) + X[5],
+        X[0] + ((X[2] - 2) ** 2) / 2 - 1,
+        X[3] - (X[1] - 3) ** 2 - 2,
+    ]
+
 
 s = optimize.root(fun, x0)
 print(s.success, s.x)
 
 # %% Exercice 2
 
-
-def J(X):
-    x1, x2 = X
-    return (x1-1)**2+2*(x1**2-x2)**2
-
-def gradJ(X):
-    x1, x2 = X
-    dj_dx1 = 2*x1-2+8*x1**3-8*x1*x2
-    dj_dx2 = 4*(-x1**2+x2)
-    return [dj_dx1, dj_dx2]
-
-def C(X, Xp):
-    x1, x2 = X
-    xp1, xp2 = Xp
-    return gradJ(X)(xp1-x1)-gradJ(X)(xp2-x2)
-
-def fun(X):
-    return [
-            gradJ(X)[1], #x+1
-            gradJ(X)[0]
-            ]
-
-def descente_gradient_fixe(X0, alpha, precision, n_max):
-    X = [X0]
-    n = 0
-    converge = False
-    while (not(converge) and n < n_max) :
-        x= X[n][0]-alpha*gradJ(X[n])[0]
-        y= X[n][1]-alpha*gradJ(X[n])[1]     
-        X.append([x,y])
-        dX = np.sqrt((X[n+1][0]-X[n][0])**2+(X[n+1][1]-X[n][1])**2)
+Xn = [-1, 2]
 
 
-        
-        n=n+1
-        converge = dX<=precision
-    return X, converge , n
-
-X0 = [-1, 2]
-alpha = 0.1
-precision = 10**-10
-n_max=10000
-
-X, converge, n = descente_gradient_fixe(X0, alpha, precision, n_max)
-
-print("Point initial :", X0)
-print("Point final :", X[-1])
-print("Convergence :", converge)
-print("Nombre d'iteration :", n)
-
-x, y = [], []
-for i in range(len(X)):
-    x.append(X[i][0])
-    y.append(X[i][1])
+def J(x, y):
+    return (x - 1) ** 2 + 2 * (x**2 - y) ** 2
 
 
+def dJ(x, y):
+    dJ_x = 2 * (x - 1) + 8 * x * (x**2 - y)
+    dJ_y = -4 * (x**2 - y)
+    return [dJ_x, dJ_y]
 
-xmin, xmax, nx = -2.5, 2.5, 101
-ymin, ymax, ny = -2.5, 2.5, 101
+
+def C(x, y, xn, yn):
+    dJ_xn, dJ_yn = dJ(xn, yn)
+    return dJ_yn * (x - xn) - dJ_xn * (y - yn)
 
 
-x1d = np.linspace(xmin,xmax,nx)
-y1d = np.linspace(ymin,ymax,ny)
-x2d, y2d = np.meshgrid(x1d,y1d)
+def dC(xn, yn):
+    dJ_xn, dJ_yn = dJ(xn, yn)
+    dC_x = dJ_yn
+    dC_y = -dJ_xn
+    return [dC_x, dC_y]
 
-nIso = 201
-plt.contour(x2d,y2d,J([x2d,y2d]),nIso)
-plt.plot(x, y, 'x-')
-plt.title('Isovaleurs')
-plt.xlabel('Valeurs de x')
-plt.ylabel('Valeurs de y')
+
+def system(X, param):
+    x = X[0]
+    y = X[1]
+    lam = X[2]
+    [xn, yn] = param
+    dJ_x, dJ_y = dJ(x, y)
+    dC_xn, dC_yn = dC(xn, yn)
+    return [dJ_x + lam * dC_xn, dJ_y + lam * dC_yn, C(x, y, xn, yn)]
+
+
+X_ini = [0, 0, 0]
+
+# initial
+print(f"X0 = {(Xn[0],Xn[1])}")
+print(f"value J(X0) = {J(Xn[0],Xn[1])}")
+plt.plot(Xn[0], Xn[1], "o")
+# first
+sol = optimize.root(system, X_ini, args=Xn)
+print(f"success : {sol.success} X = ({sol.x[0]:.3},{sol.x[1]:.3}),lam= {sol.x[2]:.3}")
+print(f"value J(X1) = {J(sol.x[0],sol.x[1])}")
+plt.plot(sol.x[0], sol.x[1], "o")
+# second
+sol = optimize.root(system, X_ini, args=[sol.x[0], sol.x[1]])
+print(f"success : {sol.success} X = ({sol.x[0]:.3},{sol.x[1]:.3}),lam= {sol.x[2]:.3}")
+print(f"value J(X2) = {J(sol.x[0],sol.x[1])}")
+plt.plot(sol.x[0], sol.x[1], "o")
+# third
+sol = optimize.root(system, X_ini, args=[sol.x[0], sol.x[1]])
+print(f"success : {sol.success} X = ({sol.x[0]:.3},{sol.x[1]:.3}),lam= {sol.x[2]:.3}")
+print(f"value J(X3) = {J(sol.x[0],sol.x[1])}")
+plt.plot(sol.x[0], sol.x[1], "o")
+
+
+# Définition du domaine de tracé
+xmin, xmax, nx = -1.5, 2.5, 550
+ymin, ymax, ny = -1.5, 2.5, 550
+
+# Discrétisation du domaine de tracé
+x1d = np.linspace(xmin, xmax, nx)
+y1d = np.linspace(ymin, ymax, ny)
+x2d, y2d = np.meshgrid(x1d, y1d)
+
+# Tracé des isovaleurs de fl
+nIso = 491
+plt.contour(x2d, y2d, J(x2d, y2d), nIso)
+plt.title("Isovaleurs")
+plt.xlabel("Valeurs de x")
+plt.ylabel("Valeurs de y")
 plt.grid()
-plt.axis('square')
+plt.axis("square")
 plt.show()
-
 
 
 # %%
